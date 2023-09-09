5fc7df7e0771509d73e1ddebf9d3e66ad1b8d709
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-09 19:28:08 +0300

    Refactoring App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,23 @@ import {Rating} from "./components/Rating/Rating";
 function App() {
     return (
         <div>
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My friends"}/>
+            <Rating value={3}/>
+            <Accordion title={"Меню"}/>
+            <Accordion title={"Title 2"}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    return <>This is APP component</>
+function PageTitle(props: {title: string}) {
+    return <h1>{props.title}</h1>
 }
 
 
