fcd48e95389089c4a81a55a7d71cc166a5f61bd6
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-09 11:06:50 +0300

    Created Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -21,4 +21,13 @@ function Rating() {
   )
 }
 
+function Accordion() {
+    return <div>
+        <h3>Меню</h3>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </div>
+}
+
 export default App;
