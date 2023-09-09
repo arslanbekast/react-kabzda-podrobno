1fd3e76dab11c722970bbc4fbb9dd745340bee31
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-09 19:28:01 +0300

    Refactoring Rating component

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,19 +1,66 @@
 import React from "react";
 
-export function Rating() {
-    return (
-        <>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </>
-    )
+export function Rating(props: {value: number}) {
+
+    if (props.value === 1) {
+        return <div>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+    }
+    if (props.value === 2) {
+        return <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    }
+    if (props.value === 3) {
+        return <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    }
+    if (props.value === 4) {
+        return <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+        </div>
+    }
+    if (props.value === 5) {
+        return <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+        </div>
+    }
+
+    return <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
 }
 
-function Star() {
-    return (
-        <div>Star</div>
-    )
+function Star(props:{selected: boolean}) {
+    if (props.selected) {
+        return <span><b>star</b> </span>
+    } else {
+        return <span>star </span>
+    }
 }
