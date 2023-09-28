10722d98d40952b65ef8419c0b3d0fb913dfddda
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-28 20:33:41 +0300

    Changed UncontrolledAccordion component

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -8,25 +8,26 @@ export function UncontrolledAccordion(props: AccordionPropsType) {
 
     const [collapsed, setCollapsed] = useState(false)
 
-    const onCLickHandler = () => {
+    const changeCollapsed = () => {
         setCollapsed(!collapsed);
     }
 
     return <div>
-        <AccordionTitle title={props.title}/>
-        <button onClick={onCLickHandler}>
-            {collapsed ? "Expand": "Collapse"}
-        </button>
+        <AccordionTitle title={props.title} changeCollapsed={changeCollapsed}/>
+        {/*<button onClick={onCLickHandler}>*/}
+        {/*    {collapsed ? "Expand": "Collapse"}*/}
+        {/*</button>*/}
         { !collapsed && <AccordionBody/> }
     </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
+    changeCollapsed: ()=>void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-    return <h3>{props.title}</h3>
+    return <h3 onClick={props.changeCollapsed} style={{cursor:"pointer"}}>{props.title}</h3>
 }
 
 function AccordionBody() {
