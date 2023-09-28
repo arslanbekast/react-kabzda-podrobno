7b74843a6242ccd0b07c6511df0208693966ad26
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-28 20:27:25 +0300

    Added UncontrolledAccordion component

---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    title: string
+}
+
+export function UncontrolledAccordion(props: AccordionPropsType) {
+
+    const [collapsed, setCollapsed] = useState(false)
+
+    const onCLickHandler = () => {
+        setCollapsed(!collapsed);
+    }
+
+    return <div>
+        <AccordionTitle title={props.title}/>
+        <button onClick={onCLickHandler}>
+            {collapsed ? "Expand": "Collapse"}
+        </button>
+        { !collapsed && <AccordionBody/> }
+    </div>
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h3>{props.title}</h3>
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
