04a59bc18dbfe86110da0ad3bee0d8b5dce8a914
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-05 22:31:16 +0300

    Changed Accordion component

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -3,22 +3,24 @@ import React, {useState} from "react";
 type AccordionPropsType = {
     title: string
     collapsed: boolean
+    onChange: () => void
 }
 
 export function Accordion(props: AccordionPropsType) {
 
     return <div>
-        <AccordionTitle title={props.title}/>
+        <AccordionTitle title={props.title} onChange={props.onChange}/>
         { !props.collapsed && <AccordionBody/> }
     </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
+    onChange: () => void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-    return <h3>{props.title}</h3>
+    return <h3 onClick={props.onChange}>{props.title}</h3>
 }
 
 function AccordionBody() {
