9d78ce69a9bc36417dffdcc0ff467e1c458ba550
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-05 20:59:23 +0300

    Changed UncontrolledRating component

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -6,21 +6,24 @@ export function UncontrolledRating() {
     const [value, setValue] = useState(0)
 
     return <div>
-        <Star selected={ value > 0 } changeRatingValue={()=>setValue(1)}/>
-        <Star selected={ value > 1 } changeRatingValue={()=>setValue(2)}/>
-        <Star selected={ value > 2 } changeRatingValue={()=>setValue(3)}/>
-        <Star selected={ value > 3 } changeRatingValue={()=>setValue(4)}/>
-        <Star selected={ value > 4 } changeRatingValue={()=>setValue(5)}/>
+        <Star selected={ value > 0 } setValue={setValue} value={1}/>
+        <Star selected={ value > 1 } setValue={setValue} value={2}/>
+        <Star selected={ value > 2 } setValue={setValue} value={3}/>
+        <Star selected={ value > 3 } setValue={setValue} value={4}/>
+        <Star selected={ value > 4 } setValue={setValue} value={5}/>
     </div>
 }
-
+type ValuesType = 1 | 2 | 3 | 4 | 5
 type StarPropsType = {
     selected: boolean
-    changeRatingValue: ()=>void
+    value: ValuesType
+    setValue: (value: ValuesType)=>void
 }
 
 function Star(props: StarPropsType) {
-    return props.selected
-        ? <span onClick={props.changeRatingValue} style={{cursor:"pointer"}}><b>star</b> </span>
-        : <span onClick={props.changeRatingValue} style={{cursor:"pointer"}}>star </span>
+    return (
+        <span onClick={() => props.setValue(props.value)} style={{cursor:"pointer"}}>
+            { props.selected ? <b>star </b> : 'star ' }
+        </span>
+    )
 }
