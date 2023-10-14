020862010f6a938cfe87a1ba7d719a27c86d5b11
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-14 19:02:00 +0300

    Created Accordion.stories for Storybook

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,45 @@
+// import type {Meta, StoryObj} from "@storybook/react";
+import {action} from "@storybook/addon-actions"
+import {Accordion} from "./Accordion";
+import React, {useState} from "react";
+
+// const meta: Meta<typeof  Accordion> = {
+//     component: Accordion
+// }
+// export default meta;
+
+export default {
+    component: Accordion
+}
+
+
+
+// type Story = StoryObj<typeof  Accordion>
+//
+// export const FirstStory: Story = {
+//     args: {
+//         title: 'bla',
+//         collapsed: true
+//     }
+// }
+
+const onChangeHandler = action('onChange')
+
+export const CollapsedAccordion = () => {
+    return <Accordion title={"Collapsed Accordion"}
+                      collapsed={true}
+                      onChange={onChangeHandler}/>
+}
+
+export const OpenedAccordion = () => {
+    return <Accordion title={"Opened Accordion"}
+                      collapsed={false}
+                      onChange={() => {}}/>
+}
+
+export const AccordionDemo = () => {
+    const [collapsed, setCollapsed] = useState(false)
+    return <Accordion title={"Accordion"}
+                      collapsed={collapsed}
+                      onChange={() => {setCollapsed(!collapsed)}}/>
+}
