e0c6406e51cd2a8feb1d9327568ee9b6dd4729a9
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-18 21:27:48 +0300

    Created Clock stories

---
+++ src/components/Clock/Clock.stories.tsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from "react";
+import {Clock} from "./Clock";
+
+export default {
+    component: Clock
+}
+
+export const BaseExample = () => {
+
+    return <Clock />
+}
