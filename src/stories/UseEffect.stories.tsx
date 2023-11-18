dd839c6c0a86c9bedb815cb76cd682ea2b636085
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-11-18 20:41:48 +0300

    Changed UseEffect stories

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -37,3 +37,34 @@ export const SimpleExample = () => {
         </>
     )
 }
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+
+        // setTimeout(() => {
+        //     console.log('setTimeout')
+        //     document.title = counter.toString()
+        // }, 1000)
+
+        setInterval(() => {
+            console.log("tick: " + counter)
+            setCounter(state => state + 1)
+        }, 1000)
+
+    }, [])
+
+
+
+    return (
+        <>
+            Hello, counter: {counter} - fake: {fake}
+            {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+            {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+        </>
+    )
+}
